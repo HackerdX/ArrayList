89757931ab8c2b4385954db88b21f9b99442f0d2
HackerdX <hackerx@192.168.1.2>
2022-08-30 23:18:54 +0530

    Added Insert and Delete functionality on Linked List c++

--- linked_list_c++/LinkedList.cpp
+++ linked_list_c++/LinkedList.cpp
@@ -2,24 +2,78 @@
 using namespace std;
 class Node{
     public:
+        static int linkedListSize;
         int data;
         Node *next;
         Node(int data){
             this->data = data;
             this->next = NULL;
         }
+        void incrementSize() {
+            linkedListSize++;
+        }
+        void decrementSize() {
+            if(linkedListSize <= 0) linkedListSize = 0;
+            linkedListSize--;
+        }
+        int size(){
+            return linkedListSize;
+        }
 };
+
+int Node::linkedListSize = 0; /* This is important for static variables */
+
+Node* insertNode(Node *head, int position, int value){
+    if(head == NULL) return NULL;
+    if(position > head->size()) position = head->size() + 1;
+    Node* newNode = new Node(value);
+    head->incrementSize();
+    if(position == 0) {
+        newNode->next = head;
+        return newNode;
+    } else{
+        int loop = position - 2;
+        Node *temp = head;
+        while (loop-- > 0){
+            temp = temp->next;
+        }
+        newNode->next = temp->next;
+        temp->next = newNode;
+        return head;
+    }
+}
+
+Node* deleteNode(Node *head, int position){
+    if (head == NULL) return NULL;
+    if (position >= head->size()) position = head->size() - 1;
+    head->decrementSize();
+    if (position == 0) {
+        return head->next;
+    } else {
+        int loop = position - 1;
+        Node *temp = head;
+        while (loop-- > 0) {
+            temp = temp->next;
+        }
+        temp->next = temp->next->next;
+        return head;
+    }
+}
+
 Node* createNodes(int size){
     if(size == 0) return NULL;
     Node *head, *temp;
     head = new Node(1);
+    head->incrementSize();
     temp = head;
     for(int i = 2; i <= size; i++){
         temp->next = new Node(i);
+        temp->incrementSize();
         temp = temp->next;
     }
     return head;
 }
+
 void printNodes(Node *head, int size){
     Node *temp = head;
     cout << "HEAD -> ";
@@ -30,20 +84,16 @@ void printNodes(Node *head, int size){
     cout << "NULL\n";
     return;
 }
-int getSizeOfLinkedList(Node *head){
-    if(head == NULL) return 0;
-    int count = 0;
-    Node *temp = head;
-    while(temp != NULL){
-        count++;
-        temp = temp->next;
-    }
-    return count;
-}
+
 int main(){
-    Node *head = createNodes(190);
-    int getSize = getSizeOfLinkedList(head);
-    printNodes(head, getSize);
-    cout << "Size of Linked List is: " << getSize << "\n";
+    Node *head = createNodes(10);
+    printNodes(head, head->size());
+    cout << "Size of Linked List is: " << head->size() << "\n";
+    head = insertNode(head, 1, 120);
+    printNodes(head, head->size());
+    cout << "Size of Linked List is: " << head->size() << "\n";
+    head = deleteNode(head, 8);
+    printNodes(head, head->size());
+    cout << "Size of Linked List is: " << head->size() << "\n";
     return 0;
 }
