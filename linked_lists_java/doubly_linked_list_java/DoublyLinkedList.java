8652b887e4b04b99f2310f8f1312d49f376236b0
Anmol Verma <cooleasycodebyanmol@gmail.com>
2022-08-31 11:47:14 +0530

    Added Insert and delete operations

--- linked_lists_java/doubly_linked_list_java/DoublyLinkedList.java
+++ linked_lists_java/doubly_linked_list_java/DoublyLinkedList.java
@@ -3,12 +3,12 @@ public class DoublyLinkedList {
         Node head = createNodes(10);
         printNodes(head, head.size());
         System.out.println("Size of Linked List is: "+ head.size());
-//        head = insertNode(head, 11, 120);
-//        printNodes(head, head.size());
-//        System.out.println("Size of Linked List is: "+ head.size());
-//        head = deleteNode(head, 13);
-//        printNodes(head, head.size());
-//        System.out.println("Size of Linked List is: "+ head.size());
+        head = insertNode(head, 11, 120);
+        printNodes(head, head.size());
+        System.out.println("Size of Linked List is: "+ head.size());
+        head = deleteNode(head, 12);
+        printNodes(head, head.size());
+        System.out.println("Size of Linked List is: "+ head.size());
     }
 
     private static Node insertNode(Node head, int position, int value){
@@ -17,6 +17,7 @@ public class DoublyLinkedList {
         Node newNode = new Node(value);
         head.incrementSize();
         if(position == 0) {
+            head.prev = newNode;
             newNode.next = head;
             return newNode;
         } else{
@@ -25,7 +26,13 @@ public class DoublyLinkedList {
             while (loop-- > 0){
                 temp = temp.next;
             }
-            newNode.next = temp.next;
+            if(temp.next == null){
+                newNode.next = null;
+            } else {
+                newNode.next = temp.next;
+                temp.next.prev = newNode;
+            }
+            newNode.prev = temp;
             temp.next = newNode;
             return head;
         }
@@ -36,14 +43,20 @@ public class DoublyLinkedList {
         if (position >= head.size()) position = head.size() - 1;
         head.decrementSize();
         if (position == 0) {
+            head.next.prev = null;
             return head.next;
         } else {
             int loop = position - 1;
             Node temp = head;
-            while (loop-- > 0) {
+            while (loop-- >= 0) {
                 temp = temp.next;
             }
-            temp.next = temp.next.next;
+            if(temp.next != null){
+                temp.next.prev = temp.prev;
+            }
+            temp.prev.next = temp.next;
+            temp.next = null;
+            temp.prev = null;
             return head;
         }
     }
@@ -65,8 +78,7 @@ public class DoublyLinkedList {
         Node head = new Node(1);
         head.incrementSize();
         Node temp = head;
-        /* Connect head to temp */
-        for(int i = 3; i <= size; i++){
+        for(int i = 2; i <= size; i++){
             Node newNode = new Node(i);
             temp.next = newNode;
             newNode.prev = temp;
